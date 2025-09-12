be3046d99135b843c19e31d9b4bdafc681291944
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-09-12 11:07:09 +0500

    Task2

---
+++ Tyuiu.AntonovDI.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.AntonovDI.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.AntonovDI.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckMessageValid()
+        {
+
+            var name = "Даниил";
+            var res = DataService.GetMessage(name);
+
+
+            Assert.AreEqual("Привет, Даниил", res);
+        }
+    }
+}
